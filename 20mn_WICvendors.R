3f402ac0de2f909644e787de4a58b8bf8b48538e
ellienyc <148107694+ellienyc@users.noreply.github.com>
2023-12-03 20:20:22 -0500

    R work for WIC vendors

---
+++ 20mn_WICvendors.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(janitor)
+library(stringr) #load libraries up top
+
+WIC_vendors <- read_csv("WIC vendors.csv") %>% #read in the csv
+  clean_names() %>% 
+  mutate(lat = str_extract(location,  "(?<=\\().*?(?=,)")) %>% #getting lat and long from the "location" column which has everything in 1
+  mutate(long = str_extract(location, "-[^)]+(?=[)])")) #code from chatGPT https://chat.openai.com/share/2d5b9f60-0c01-4c67-8530-5eaa3b120c9e
+  
+grocery_WIC <- WIC_vendors %>% #the vendors list has grocery, pharmacy, and both so looking for the ones that are not pharmacy here
+  filter (type != "Pharmacy Only")
+
+# write_csv(grocery_WIC, "grocery_WIC.csv")
+
+pharmacy_WIC <- WIC_vendors %>% #^^and the ones that aren't grocery here
+  filter(type != "Grocery")
+
+# write_csv(pharmacy_WIC, "pharmacy_WIC.csv")
+
+
